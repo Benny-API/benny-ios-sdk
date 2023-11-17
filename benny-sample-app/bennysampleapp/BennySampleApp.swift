49fa8984eb287990c5794edc8f9753860b85cc37
Ariel Bong <ariel@bennyapi.com>
2023-11-17 15:00:08 -0500

    Add sample app

---
+++ benny-sample-app/bennysampleapp/BennySampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BennySampleApp.swift
+//  bennysampleapp
+//
+//  Created by Ariel Bong on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct BennySampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ benny-sample-app/bennysampleapp/ExampleListener.swift
@@ -0,0 +1,28 @@
+//
+//  ExampleListener.swift
+//  bennysampleapp
+//
+//  Created by Ariel Bong on 11/17/23.
+//
+
+import Foundation
+import BennySDK
+import SwiftUI
+
+class ExampleBennyApplyListener: BennyApplyListenerDelegate {
+    var isPresentingView: Binding<Bool>
+    
+    init(isPresentingView: Binding<Bool>) {
+        self.isPresentingView = isPresentingView
+    }
+    
+    func onExit() {
+        print("exiting flow")
+        isPresentingView.wrappedValue.toggle()
+    }
+    
+    func onDataExchange(applicantDataId: String) {
+        print("data exchange commenced")
+        print(applicantDataId)
+    }
+}
